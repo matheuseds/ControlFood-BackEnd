df9dfdfbce74de751e40653e2132aeb1c8f5af54
JoaoFlavioRodrigues <64814225+Joaorodrigues7132@users.noreply.github.com>
2022-03-27 22:00:56 -0300

    xxx
    ccc

--- src/views/dev.view.js
+++ src/views/dev.view.js
@@ -1,9 +1,17 @@
-import Express, { Router } from "express";
+import Express from "express";
+import conexao from "../database/conexao"
 
 const router = Express.Router();
+
 router.get("/conexaoBackEnd", (req, res) => {
   const mensagem = "Acessar back end";
   res.status(200).json({ mensagem });
 });
+
+router.get("/conexaobanco", async (req, res) => {
+  const mensagem = await conexao.testaconexao() ;
+  res.status(200).json({ mensagem });
+});
+
 
 module.exports = router;
