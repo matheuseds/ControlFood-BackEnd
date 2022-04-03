711619afde98b289c9ae2379d5cf7b3e5af68f9d
Matheus Eduardo da Silva <71354813+matheuseds@users.noreply.github.com>
2022-04-03 18:32:28 -0300

    Fix cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const port = 8080;
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
